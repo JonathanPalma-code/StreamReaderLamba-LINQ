94217ae1a171c7a3db57892f7771d7cdafd62532
Jonathan <jonathanpalma89@gmail.com>
2020-03-27 19:06:33 +0000

    chore: add streamReader, Lambda expression and LINQ

--- StreamReaderLambaAndLINQ/Program.cs
+++ StreamReaderLambaAndLINQ/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using StreamReaderLambaAndLINQ.Entities;
+using System.Linq;
 
 namespace StreamReaderLambaAndLINQ
 {
@@ -6,7 +10,35 @@ namespace StreamReaderLambaAndLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string[] fields = streamReader.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2]);
+
+                    employees.Add(new Employee(name, email, salary));
+                }
+            }
+
+            Console.Write("Enter the salary: ");
+            double nSalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Email of people whose salary is more than £{nSalary.ToString("F2")}:");
+            var result = employees.Where(e => e.Salary > nSalary).OrderBy(e => e.Email).Select(e => e.Email);
+            foreach (string employee in result)
+            {
+                Console.WriteLine(employee);
+            }
+            var salaryWm = employees.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.WriteLine("Sum of people's salary whose name starts with 'M': £" + salaryWm.ToString("F2") + ".");
         }
     }
 }
